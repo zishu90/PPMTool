f303a0dd1356aa3e7623047ed0c1409a2f856345
Zeeshan Ahmad97 <zeeahm97@in.ibm.com>
2021-06-29 11:06:33 +0530

    first commit

---
+++ ppmt/src/main/java/com/example/ppmtback/PpmtBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppmtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtBackApplication.class, args);
+    }
+
+}
---
+++ ppmt/src/test/java/com/example/ppmtback/PpmtBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppmtback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
